6117b46d2091576c1dfdc4692a345fb55b9abde4
FrankyRay <frankyraymarcellsinaga@gmail.com>
2025-01-23 23:09:55 +0700

    rm: bible; feat: jokes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,50 @@
 import Image from "next/image";
 import style from "@/app/page.module.css";
-import Bible from "@/lib/components/Bible";
 import { lexendLight, lexendMedium } from "@/lib/fonts";
+import Jokes from "@/lib/components/Jokes";
 
 export default function Home() {
-  return <>
-    <div className={ style.mainHome }>
-      <div className={`${style.message}`}>
-        <span className={`${style.title} ${lexendMedium.className}`}>
-          Welcome to <b>frays.dev</b>
-        </span>
-        <span className={`${lexendLight.className}`}>
-          Personal website of FrankyRayMS
-        </span>
+  return (
+    <>
+      <div className={style.mainHome}>
+        <div className={`${style.message}`}>
+          <span className={`${style.title} ${lexendMedium.className}`}>
+            Welcome to <b>frays.dev</b>
+          </span>
+          <span className={`${lexendLight.className}`}>
+            Personal website of FrankyRayMS
+          </span>
+        </div>
+
+        <div className={`${style.subinfo} ${lexendLight.className}`}>
+          Page under construction
+        </div>
       </div>
 
-      <div className={`${style.subinfo} ${lexendLight.className}`}>
-        Page under construction
-      </div>
-    </div>
+      <div className={style.footer}>
+        <div className={style.linkIcon}>
+          <a href="https://github.com/FrankyRay" target="_blank">
+            <Image
+              src="/icons/github-mark-white.svg"
+              width={20}
+              height={20}
+              alt="Github icon redirect into Github page"
+            />
+          </a>
+          <a href="https://twitter.com/frays_csui" target="_blank">
+            <Image
+              src="/icons/x-twitter.svg"
+              width={20}
+              height={20}
+              alt="X (formerly Twitter) icon redirect into X page"
+            />
+          </a>
+        </div>
 
-    <div className={style.footer}>
-      <div className={ style.linkIcon }>
-        <a href="https://github.com/FrankyRay" target="_blank">
-          <Image 
-            src="/icons/github-mark-white.svg"
-            width={ 20 }
-            height={ 20 }
-            alt="Github icon redirect into Github page"
-          />
-        </a>
-        <a href="https://twitter.com/frays_csui" target="_blank">
-          <Image 
-            src="/icons/x-twitter.svg"
-            width={ 20 }
-            height={ 20 }
-            alt="X (formerly Twitter) icon redirect into X page"
-          />
-        </a>
+        <div className={style.bibleInfo}>
+          <Jokes />
+        </div>
       </div>
-
-      <div className={ style.bibleInfo }>
-        <Bible />
-      </div>
-    </div>
-  </>;
+    </>
+  );
 }
--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,11 +1,15 @@
-import { Lexend } from "next/font/google";
+import { Geist_Mono, Lexend } from "next/font/google";
 
 export const lexendMedium = Lexend({
-    weight: "500",
-    subsets: [ 'latin' ]
-})
+  weight: "500",
+  subsets: ["latin"],
+});
 
 export const lexendLight = Lexend({
-    weight: "300",
-    subsets: [ 'latin' ]
-})
+  weight: "300",
+  subsets: ["latin"],
+});
+
+export const geistMono = Geist_Mono({
+  weight: "500",
+});
