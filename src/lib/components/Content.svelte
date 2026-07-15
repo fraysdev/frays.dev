74438c02adb716c0bbf70537bb8dab8d481043bb
fraysdev <61337607+fraysdev@users.noreply.github.com>
2026-07-15 15:43:11 +0700

    add: add missing component

---
+++ src/lib/components/Content.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+	let { children } = $props();
+</script>
+
+<div class="page-content">
+    <Header />
+    <div class="content">
+        {@render children()}
+    </div>
+    <Footer />
+</div>
+
+<style>
+    .page-content {
+        border: 1px solid var(--color-border);
+        background-color: var(--color-bg);
+    }
+</style>
